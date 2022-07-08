59459e565f1c39a1e9f630f4cccb38cf6948e96e
AndreiGrb <AndreiGrb@yandex.ru>
2022-07-09 01:08:05 +0300

    UE4 project import

---
+++ Source/AndreiGrbCodeExample/AndreiGrbCodeExampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AndreiGrbCodeExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANDREIGRBCODEEXAMPLE_API AAndreiGrbCodeExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
