bcff20438a5284dfec06f74caf3e694d1db3323c
AndreiGrb <AndreiGrb@yandex.ru>
2022-07-09 22:36:08 +0300

    Player Starting Zone

---
+++ Source/AndreiGrbCodeExample/Sandbox/Miscellaneous/AGCEPlayerStartZone.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AGCEPlayerStartZone.h"
+#include "Components/BillboardComponent.h"
+#include "Components/ShapeComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+AAGCEPlayerStartZone::AAGCEPlayerStartZone()
+{
+	ConstructorHelpers::FObjectFinder<UTexture2D> PlayerStartTextureObject(TEXT("/Engine/EditorResources/S_Player"));
+	if(PlayerStartTextureObject.Succeeded())
+		GetSpriteComponent()->Sprite = PlayerStartTextureObject.Object;
+}
+
+FVector AAGCEPlayerStartZone::GetRandomSpawnPointInZone() const
+{
+	const FVector BoxExtent = GetCollisionComponent()->GetCollisionShape().GetBox();
+	FVector RandomPoint = UKismetMathLibrary::RandomPointInBoundingBox(GetActorLocation(), BoxExtent);
+
+	RandomPoint.Z = GetActorLocation().Z;
+	
+	return RandomPoint;
+}
+
+
+
---
+++ Source/AndreiGrbCodeExample/Sandbox/Miscellaneous/AGCEPlayerStartZone.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "AGCEPlayerStartZone.generated.h"
+
+UCLASS()
+class ANDREIGRBCODEEXAMPLE_API AAGCEPlayerStartZone : public ATriggerBox
+{
+	GENERATED_BODY()
+
+public:
+	AAGCEPlayerStartZone();
+
+public:
+	UFUNCTION(BlueprintCallable)
+	FVector GetRandomSpawnPointInZone() const;
+};
